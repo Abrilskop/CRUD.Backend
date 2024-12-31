2cceb0f2322f4d5a0706073fb8b7ebb381ecf9aa
Alexandra Ramos <uberpot.ia@gmail.com>
2024-12-30 22:10:14 -0500

    chore: add cliente class

--- CRUD.Backend.Domain/Cliente/DTO/Cliente.cs
+++ CRUD.Backend.Domain/Cliente/DTO/Cliente.cs
@@ -8,5 +8,9 @@ namespace CRUD.Backend.Domain.Clientes.DTO
 {
     internal class Cliente
     {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public int Telefono { get; set; }
     }
 }
