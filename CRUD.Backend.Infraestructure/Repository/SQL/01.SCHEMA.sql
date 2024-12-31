dbe2be4e0a22f5d674815ab2300c9bcee33d519e
Alexandra Ramos <uberpot.ia@gmail.com>
2024-12-31 18:52:53 -0500

    feat: add list procedures and getbyid client

--- CRUD.Backend.Infraestructure/Repository/SQL/01.SCHEMA.sql
+++ CRUD.Backend.Infraestructure/Repository/SQL/01.SCHEMA.sql
@@ -1,9 +1,70 @@
-﻿CREATE TABLE cliente(
-    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellidos VARCHAR(100) NOT NULL,
-    direccion VARCHAR(255),
-    telefono VARCHAR(15),
-    email VARCHAR(100),
-    fecha_registro DATE DEFAULT CURRENT_DATE
+﻿create database BD_Cliente
+
+use BD_Cliente
+
+
+CREATE TABLE cliente (
+	idCliente INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellidoPaterno VARCHAR(100),
+	apellidoMaterno VARCHAR(100), 
+    telefono VARCHAR(20),
+    direccion VARCHAR(300),
+    fecha_registro DATETIME DEFAULT GETDATE()
 );
+-- alt + shift --> seleccion de varias lineas
+-- obtener lista de clientes
+create procedure [dbo].[spu_get_list_cliente]
+as
+begin
+	SELECT
+	idCliente,
+    nombre,
+    apellidoPaterno,
+	apellidoMaterno,
+    telefono,
+    direccion,
+    fecha_registro
+	FROM
+	cliente
+end
+
+-- obtener solo 1 cliente por su id
+create procedure [dbo].[spu_get_cliente_by_id]
+(
+@idCliente int
+)
+as
+begin
+	SELECT
+	idCliente,
+    nombre,
+    apellidoPaterno,
+	apellidoMaterno,
+    telefono,
+    direccion,
+    fecha_registro
+	FROM
+	cliente
+	WHERE idCliente = @idCliente
+end
+
+-- create empleado
+create procedure [dbo].[spu_create_cliente]
+(
+@idCliente int
+)
+as
+begin
+	SELECT
+	idCliente,
+    nombre,
+    apellidoPaterno,
+	apellidoMaterno,
+    telefono,
+    direccion,
+    fecha_registro
+	FROM
+	cliente
+	WHERE idCliente = @idCliente
+end
