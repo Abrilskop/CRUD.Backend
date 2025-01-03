abd28e66471afe335851ec8f25be64a7f68b61e5
Alexandra Ramos <uberpot.ia@gmail.com>
2025-01-02 22:47:28 -0500

    chore: add docIdentidad in cliente table

--- CRUD.Backend.Infraestructure/Repository/SQL/01.SCHEMA.sql
+++ CRUD.Backend.Infraestructure/Repository/SQL/01.SCHEMA.sql
@@ -9,6 +9,7 @@ CREATE TABLE cliente (
     apellidoPaterno VARCHAR(100),
 	apellidoMaterno VARCHAR(100), 
     telefono VARCHAR(20),
+	docIdentidad VARCHAR(8),
     direccion VARCHAR(300),
     fecha_registro DATETIME DEFAULT GETDATE()
 );
@@ -23,6 +24,7 @@ begin
     apellidoPaterno,
 	apellidoMaterno,
     telefono,
+	docIdentidad,
     direccion,
     fecha_registro
 	FROM
@@ -42,6 +44,7 @@ begin
     apellidoPaterno,
 	apellidoMaterno,
     telefono,
+	docIdentidad,
     direccion,
     fecha_registro
 	FROM
@@ -52,19 +55,43 @@ end
 -- create empleado
 create procedure [dbo].[spu_create_cliente]
 (
-	@idCliente INT IDENTITY(1,1) PRIMARY KEY,
     @nombre VARCHAR(100),
     @apellidoPaterno VARCHAR(100),
 	@apellidoMaterno VARCHAR(100), 
     @telefono VARCHAR(20),
+	@docIdentidad VARCHAR (8),
     @direccion VARCHAR(300),
-    @fecha_registro DATETIME DEFAULT GETDATE()
-    
+    @fecha_registro DATETIME,
+    @mssError VARCHAR(100)
 )
 as
 begin
-	IF (EXIST (SELECT * FROM cliente WHERE idCliente = @idCliente))
+	IF (EXISTS (SELECT docIdentidad FROM cliente WHERE docIdentidad = @docIdentidad)) -- valido si tiene el mismo DNI
     begin
-        set @mssError
+        set @mssError = 'El cliente ya se encuentraesta registrado'
+        return
     end
+		
+	INSERT INTO cliente
+	(
+		nombre,
+		apellidoPaterno,
+		apellidoMaterno,
+		telefono,
+		docIdentidad,
+		direccion,
+		fecha_registro
+	)
+	VALUES
+	(
+		@nombre,
+		@apellidoPaterno,
+		@apellidoMaterno,
+		@telefono,
+		@docIdentidad,
+		@direccion,
+		@fecha_registro
+	)
+	set @mssError ='Cliente insertado'
+
 end
