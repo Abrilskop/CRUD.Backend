9fd2f1812a9e8ba2c13f4964b9da89fa385fedd6
Alexandra Ramos <uberpot.ia@gmail.com>
2025-01-04 22:36:08 -0500

    chore: add prop clienteApp

--- CRUD.Backend/Controllers/ClienteController.cs
+++ CRUD.Backend/Controllers/ClienteController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CRUD.backend.Aplication.Clientes;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace CRUD.Backend.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ClienteController : ControllerBase
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        private readonly ClienteApp _customerApp;
+
+        public ClienteController(
+            IWebHostEnvironment webHostEnvironment,
+
+            ClienteApp clienteApp,
+
+        )
+        {
+            this._webHostEnvironment = webHostEnvironment;
+            this._customerApp = clienteApp;
+
+
+            //Set culture
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-PE");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("es-PE");
+            Thread.CurrentThread.CurrentUICulture.DateTimeFormat.FirstDayOfWeek = DayOfWeek.Monday;
+        }   
     }
 }
--- CRUD.backend.Aplication/Cliente/ClienteApp.cs
+++ CRUD.backend.Aplication/Cliente/ClienteApp.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace CRUD.backend.Aplication.Clientes
 {
-    internal class ClienteApp
+    public class ClienteApp
     {
+        public ClienteApp() { }
     }
 }
